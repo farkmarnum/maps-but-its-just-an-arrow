463ef2149f3371b57546901f42ce4d40fc66c637
farkmarnum <mark.farnum@gmail.com>
2021-02-06 22:55:48 -0500

    upgrade typescript, install preact, setup, move directories (WIP)

---
+++ frontend/src/components/app.tsx
@@ -0,0 +1,11 @@
+import { FunctionalComponent, h } from 'preact';
+
+const App: FunctionalComponent = () => {
+  return (
+    <div id="app">
+      test
+    </div>
+  );
+};
+
+export default App;
